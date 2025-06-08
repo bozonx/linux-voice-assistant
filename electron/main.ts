18ab21efb102403fa06642b5ced285b46974b725
Ivan <ipkozyrin@gmail.com>
2025-06-07 21:41:55 -0300

    add

--- electron/main.ts
+++ electron/main.ts
@@ -63,7 +63,7 @@ function createWindow() {
   });
 
   if (process.env.NODE_ENV === "development") {
-    mainWindow.loadURL("http://localhost:5173");
+    mainWindow.loadURL("http://localhost:3000");
   } else {
     mainWindow.loadFile(path.join(__dirname, "../dist/index.html"));
   }
