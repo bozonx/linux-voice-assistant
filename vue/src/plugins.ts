e7c671ba2262400d03b157fa868981071ea45c3e
Ivan <ipkozyrin@gmail.com>
2025-07-06 10:19:20 -0300

    add

--- vue/src/plugins.ts
+++ vue/src/plugins.ts
@@ -1,8 +1,10 @@
 import usePluginContext from "./composables/usePluginContext";
 import SearchInInternet from "./plugins/SearchInInternet";
+import RussianStress from "./plugins/RussionStress";
 
 export default function usePlugins() {
   const { use } = usePluginContext();
 
   use(SearchInInternet);
+  use(RussianStress);
 }
---
+++ vue/src/plugins/RussionStress/index.ts
@@ -0,0 +1,13 @@
+import { PluginContext } from "../../types/PluginContext";
+
+export default function pluginIndex(ctx: PluginContext) {
+  // ctx.registerEditItems([
+  //   {
+  //     name: "Russian Stress",
+  //     action: async (text: string) => {
+  //       console.log(text);
+  //       return text;
+  //     },
+  //   },
+  // ]);
+}
