81bd0cc24fe0ca886f6665d95754dad255c80b1a
Ivan Kozyrin <ipkozyrin@gmail.com>
2026-04-22 11:18:53 -0300

    refactor

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": resolve(__dirname, "src"),
-      "@shared": resolve(__dirname, "../../packages/shared/src"),
+      "@shared": resolve(__dirname, "../packages/shared/src"),
     },
   },
   // resolve: {
